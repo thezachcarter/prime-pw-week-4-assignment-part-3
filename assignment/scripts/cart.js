06202d3d510f853f6ddd852b24c5500d9044c239
Zach Carter <thezachcarter@gmail.com>
2022-02-12 08:16:48 -0600

    completed stretch2

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,10 +2,31 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [ ]
+const maxItems = 5
 
+/*
 function addItem( item ){
   ( basket.push( item ) );
   return true;
+}//end addItem function
+console.log( `basket is ${basket}`);
+*/
+// saved for reference and in case I break something
+
+function isFull( basket, maxItems ){
+  if ( basket.length <= maxItems ){
+    return true;
+  } else {
+    console.log ('Too many items in basket!');
+    return false;
+  }// end if else
+}//end full function
+
+function addItem( item ){
+  if ( isFull( basket , maxItems ) == false ){
+    return false; }
+    else { basket.push( item );
+    return true; }// end if else
 }//end addItem function
 console.log( `basket is ${basket}`);
 
@@ -40,6 +61,18 @@ addItem( 'lettuce' );
 console.log( `basket is ${basket}`);
 
 addItem( 'tomato' );
+console.log( `basket is ${basket}`);
+
+addItem( 'bread' );
+console.log( `basket is ${basket}`);
+
+addItem( 'mayo' );
+console.log( `basket is ${basket}`);
+
+addItem( 'avocado' );
+console.log( `basket is ${basket}`);
+
+addItem( 'jalapeno' );
 console.log( `basket is ${basket}`);
 
 console.log( listItems( basket ) );
