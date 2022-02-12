6a8592e7069589fc1f23d67030adf09d5b6435da
Zach Carter <thezachcarter@gmail.com>
2022-02-12 07:19:13 -0600

    completed basic requirments

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,7 +4,7 @@ console.log('***** Cart Functions *****');
 let basket = [ ]
 
 function addItem( item ){
-  ( basket.push(item) );
+  ( basket.push( item ) );
   return true;
 }//end addItem function
 console.log( `basket is ${basket}`);
@@ -20,10 +20,29 @@ console.log( `basket is ${basket}`);
 
 function listItems( basket ){
   for( item of basket){
-    console.log(item);
+    console.log( item );
   }//end for of loop
 }//end listItems function
 
-function empty ( basket ){
+console.log( listItems( basket ) );
 
+function empty ( basket ){
+  (basket.length = 0);
 }//end empty function
+
+empty( basket );
+console.log( `basket is ${basket}`);
+
+addItem( 'bacon' );
+console.log( `basket is ${basket}`);
+
+addItem( 'lettuce' );
+console.log( `basket is ${basket}`);
+
+addItem( 'tomato' );
+console.log( `basket is ${basket}`);
+
+console.log( listItems( basket ) );
+
+empty( basket );
+console.log( `basket is ${basket}`);
